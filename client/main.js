45c614c6e3e0366e0788d72fc774155e2d703ed0
Yinghua009 <emlizhu09@gmail.com>
2023-12-18 16:16:25 +0100

    initial commit base search

---
+++ client/main.js
@@ -0,0 +1,37 @@
+// Declare a new function named search
+async function search() {
+  // read the user input from the term field in the form searchForm
+  let searchTerm = document.forms.searchForm.term.value;
+  // read the searchType
+  let searchType = document.forms.searchForm.searchType.value;
+  console.log(searchType);
+  // empty the input field
+  document.forms.searchForm.term.value = '';
+  // read the json
+  let rawData = await fetch('/api/music/' + searchTerm + '/' + searchType);
+  // convert json to a javascript data structure
+  let songs = await rawData.json();
+  // create an variable name that initially is an empty string
+  let html = `
+    <p>Du sökte efter "${searchTerm}"...</p>
+    <p>Hittade ${songs.length} låtar.</p>
+  `;
+  // loop through the found songs
+  for (let song of songs) {
+    let meta = song.metadata.common;
+    html += `
+      <section>
+        <h2>${meta.title}</h2>
+        <p><b>Artist:</b> ${meta.artist}</p>
+        <p><b>Album:</b> ${meta.album}</p>  
+        <p>
+          <audio controls src="music/${song.fileName}">
+        </p>
+      </section>
+    `;
+  }
+  // Grab the element/tag with the class searchResults
+  let searchResultsElement = document.querySelector('.searchResults');
+  // Change the content of the searchResults element
+  searchResultsElement.innerHTML = html;
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+// Import the express package/module
+// that let us start up a web server
+import express from 'express';
+
+// Import the database driver
+import mysql from 'mysql2/promise';
+
+// Create a web server named app
+const app = express();
+
+// Serve all files in the folder client
+app.use(express.static('client'));
+
+// Start the server on a certain port
+// and write to the terminal which port...
+app.listen(3000, () =>
+  console.log('Listening on http://localhost:3000'));
+
+
+
+// Create a connection 'db' to the database
+const db = await mysql.createConnection({
+  host: '161.97.144.27',
+  port: '8093', // your port here eventually
+  user: 'root',
+  password: 'guessagain93', // your password here eventually
+  database: 'MetaHub'
+});
+
+// A small function for a database query
+async function query(sql, listOfValues) {
+  let result = await db.execute(sql, listOfValues);
+  return result[0];
+}
+
+
+// A search route to find music
+app.get('/api/music/:searchTerm/:searchType', async (request, response) => {
+  // Get the search term from as a parameter from the route/url
+  let searchTerm = request.params.searchTerm;
+  // Get teh search type a sa parameter from the route/url
+  let searchType = request.params.searchType;
+  // Make a database query and remember the result
+  // using the search term
+
+  let sql = `
+   SELECT * 
+   FROM music
+   WHERE LOWER(metadata -> '$.common.${searchType}') LIKE LOWER (?)
+  `;
+
+  // since the sql gets a bit different if you want to search all
+  // fix this with a if-clause replacing the sql
+  if (searchType == 'all') {
+    sql = `
+      SELECT *
+      FROM music
+      WHERE LOWER(MetaHub) LIKE LOWER (?)
+    `;
+  }
+
+  let result = await query(sql, ['%' + searchTerm + '%']);
+
+  // Send a response to the client
+  response.json(result);
+});
